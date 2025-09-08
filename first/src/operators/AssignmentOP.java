1fc2a05101971086b33d29a0af9726cf16647c10
Mahigavale <maheshgavale07@gmail.com>
2025-09-08 10:51:59 +0530

    Operators:

---
+++ first/src/operators/AssignmentOP.java
@@ -0,0 +1,19 @@
+package operators;
+
+public class AssignmentOP {
+
+	public static void main(String[] args) {
+		
+		int age=23; //assignment Operator.
+		
+		System.out.println(age==23);
+		
+		String str=new String("abc");
+		
+		String str2=new String("abc");
+		
+		System.out.println(str==str2);
+
+	}
+
+}
