2dcbaff139068ad0d181163aac985febe6276755
Mahigavale <maheshgavale07@gmail.com>
2025-11-25 13:12:36 +0530

    Special:

---
+++ Arrays/src/two_dimensional/Fourth.java
@@ -0,0 +1,28 @@
+package two_dimensional;
+
+public class Fourth {
+
+	public static void main(String[] args) {
+
+
+		String str="1741725";
+		
+		int i=Integer.parseInt(str);
+		int sum=0;
+		while(i>0) {
+		int num=i%10;
+		int sum2=1;
+		for(int j=1;j<=str.length();j++)
+		{
+			sum2=sum2*num; 
+		}
+		sum=sum+sum2;
+      i=i/10;
+		}
+		
+		
+		System.out.println(str);
+		System.out.println(sum);
+	}
+
+}
---
+++ Arrays/src/two_dimensional/Second.java
@@ -0,0 +1,28 @@
+package two_dimensional;
+
+public class Second {
+
+	public static void main(String[] args) {
+		
+		/**
+		 *  *****
+		 *  ****
+		 *  ***
+		 *  **
+		 *  *
+		 */
+		
+		
+		int num2=5;
+		
+		for(int j=1; j<=num2;j++)
+		{
+			for(int k=1;k<=j;k++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
