204349697a39ead950f1db7110ab526220143010
Mahigavale <maheshgavale07@gmail.com>
2025-12-01 13:41:43 +0530

    Collection Framework

---
+++ jcf/src/listif/First.java
@@ -0,0 +1,30 @@
+package listif;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class First {
+
+	public static void main(String[] args) {
+		
+		/**
+		 * List => ArrayList,LinkedList,Vector,Stack
+		 */
+
+		
+		 List<String> list=new ArrayList<String>();
+		 
+	  System.out.println(list.add("dkdmk"));
+	  System.out.println(list.add("dkdmk"));
+		
+		 /**
+		  * List => Type Safe.
+		  */
+
+   
+	      		  
+		  System.out.println(list);
+		 
+	}
+}
+
