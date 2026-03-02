adb4ac6cc22ddf330dd53ff0d1b62ca41349c119
Mahigavale <maheshgavale07@gmail.com>
2026-03-02 12:55:16 +0530

    JDBC

---
+++ jdbc_api_2/src/basic_calls/Fourth.java
@@ -0,0 +1,36 @@
+package basic_calls;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Fourth {
+
+	public static void main(String[] args) throws SQLException {
+		
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/school", "root", "root");
+		
+		   con.setAutoCommit(false);
+		   Statement stmt=con.createStatement();
+		 //  Statement stmt2=con.createStatement();
+		   
+		   stmt.addBatch("insert into student values(111,'abc','abc@gmail.com'),(444,'xyz','abc@gmail.com');");
+		   stmt.addBatch("insert into student values(222,'abc','abc@gmail.com');");
+		   stmt.addBatch("delete from student where id=333;");
+		   
+		   int count=1;
+	int[] arr=stmt.executeBatch();
+	 for(int i:arr)
+	 {
+		 System.out.println("no.of affected for each query"+i+"Query is:"+count);
+		 count++;
+	 }
+	       con.commit();
+			
+
+	       con.close();
+	}
+
+}
